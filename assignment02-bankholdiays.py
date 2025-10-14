0a87d1845d3930238fa5360a59a367ce82e67195
unknown <dima.kozlovskyy@gmail.com>
2025-10-14 13:45:47 +0100

    test via command line

---
+++ assignment02-bankholdiays.py
@@ -0,0 +1,22 @@
+# Program gets Link with JSON format, decode it in dict and make a selection of Bank Holidays dates
+# Author: Dima Kozlovskyy 
+#
+import requests
+
+url = "https://www.gov.uk/bank-holidays.json"
+response = requests.get(url)
+data = response.json()
+
+bank_holidays_dates = []
+
+event_index = 0
+
+while event_index < len(data['northern-ireland']['events']):
+    if 'bank' in data['northern-ireland']['events'][event_index]['title']:
+        bank_holidays_dates.append(data['northern-ireland']['events'][event_index]['date'])
+        
+    event_index += 1
+
+
+    
+print(bank_holidays_dates)
